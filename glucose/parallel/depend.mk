37d773c0df501bf9c3d50c94d70b5745dd137f87
Maël Valais <mael.valais@gmail.com>
2016-10-15 18:12:14 +0200

    ocaml wrapping for glucose v4

---
+++ glucose/parallel/depend.mk
@@ -0,0 +1,142 @@
+/Users/maelv/Downloads/ocaml-glucose/glucose/parallel/ClausesBuffer.o /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/ClausesBuffer.or /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/ClausesBuffer.od /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/ClausesBuffer.op: \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/ClausesBuffer.cc \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/ClausesBuffer.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Vec.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/IntTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/XAlloc.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/SolverTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Alg.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Map.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Alloc.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Solver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Heap.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/Options.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/ParseUtils.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/BoundedQueue.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Constants.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Clone.h
+/Users/maelv/Downloads/ocaml-glucose/glucose/parallel/Main.o /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/Main.or /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/Main.od /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/Main.op: /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/Main.cc \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/System.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/IntTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/ParseUtils.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/Options.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Vec.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/XAlloc.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Dimacs.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/SolverTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Alg.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Map.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Alloc.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../simp/SimpSolver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Queue.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Solver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Heap.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/BoundedQueue.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Constants.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Clone.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/ParallelSolver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SharedCompanion.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SolverCompanion.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/ClausesBuffer.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/MultiSolvers.h
+/Users/maelv/Downloads/ocaml-glucose/glucose/parallel/MultiSolvers.o /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/MultiSolvers.or /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/MultiSolvers.od /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/MultiSolvers.op: \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/MultiSolvers.cc \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/MultiSolvers.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/ParallelSolver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/SolverTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/IntTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Alg.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Vec.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/XAlloc.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Map.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Alloc.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Solver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Heap.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/Options.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/ParseUtils.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/BoundedQueue.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Constants.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Clone.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../simp/SimpSolver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Queue.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SharedCompanion.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SolverCompanion.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/ClausesBuffer.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Sort.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/System.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SolverConfiguration.h
+/Users/maelv/Downloads/ocaml-glucose/glucose/parallel/ParallelSolver.o /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/ParallelSolver.or /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/ParallelSolver.od /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/ParallelSolver.op: \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/ParallelSolver.cc \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/ParallelSolver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/SolverTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/IntTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Alg.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Vec.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/XAlloc.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Map.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Alloc.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Solver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Heap.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/Options.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/ParseUtils.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/BoundedQueue.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Constants.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Clone.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../simp/SimpSolver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Queue.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SharedCompanion.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SolverCompanion.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/ClausesBuffer.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Sort.h
+/Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SharedCompanion.o /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SharedCompanion.or /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SharedCompanion.od /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SharedCompanion.op: \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SharedCompanion.cc \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Solver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Heap.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Vec.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/IntTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/XAlloc.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Alg.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/Options.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/ParseUtils.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/SolverTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Map.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Alloc.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/BoundedQueue.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Constants.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Clone.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/ParallelSolver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../simp/SimpSolver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Queue.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SharedCompanion.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SolverCompanion.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/ClausesBuffer.h
+/Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SolverCompanion.o /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SolverCompanion.or /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SolverCompanion.od /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SolverCompanion.op: \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SolverCompanion.cc \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SolverCompanion.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Vec.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/IntTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/XAlloc.h
+/Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SolverConfiguration.o /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SolverConfiguration.or /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SolverConfiguration.od /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SolverConfiguration.op: \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/SolverConfiguration.cc \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/MultiSolvers.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/ParallelSolver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/SolverTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/IntTypes.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Alg.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Vec.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/XAlloc.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Map.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Alloc.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Solver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Heap.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/Options.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../utils/ParseUtils.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/BoundedQueue.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../core/Constants.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Clone.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../simp/SimpSolver.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../mtl/Queue.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SharedCompanion.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SolverCompanion.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/ClausesBuffer.h \
+  /Users/maelv/Downloads/ocaml-glucose/glucose/parallel/../parallel/SolverConfiguration.h
